f90122828c4c0d63171a9b5978b23a9f1141f5b8
Anbu-Saravanan <anbu.saravanan11211@gmail.com>
2025-08-03 00:19:10 +0530

    modified health check

---
+++ src/main/java/com/anbu/ems/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.anbu.ems.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/check")
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "OK";
+    }
+}
